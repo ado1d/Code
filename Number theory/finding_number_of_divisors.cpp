88042b79621aa884f28b31bfdf60692fca123e83
ado1d <2021831024@student.sust.edu>
2024-01-17 18:37:20 +0600

    finding divisors of a number

---
+++ Number theory/finding_number_of_divisors.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//program to find number of divisors, sum of divisors & all divisors of a number
+
+void all_divisors_print(const int &n) {
+    int cnt = 0, sum = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (!(n % i)) {
+            cout << "Divisors : " << i << " & " << n / i << '\n';
+            cnt++, sum += i;
+            if (n / i != i) {
+                cnt++, sum += n / i;
+            }
+        }
+    }
+    cout << "Number of divisors : " << cnt << ' ' << "Sum of diviosrs : " << sum << '\n';
+}
+
+int main() {
+    int n;                  cin >> n;
+    all_divisors_print(n);
+}
