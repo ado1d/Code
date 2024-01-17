f4ebad92c273ea1e75ec63ce58407dd74caf13ba
ado1d <2021831024@student.sust.edu>
2024-01-17 22:04:12 +0600

    Check if the number is prime or not

---
+++ Number theory/prime_check.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(const int &n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (!(n % i)) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;                  cin >> n;
+    isPrime(n) ? cout << "Prime number\n" : cout << "Not prime number\n";
+}
