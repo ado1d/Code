c2998b8b128163cd3b597874f774f5b600a78568
ado1d <2021831024@student.sust.edu>
2024-02-04 23:33:21 +0600

    to avoid hack in contest i used custom hash

--- demo.cpp
+++ demo.cpp
@@ -14,11 +14,27 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 #define int             long long
 #define pb              push_back
 #define rep(i, a, b)    for (auto i = a; i < b; i++)
+#define rerep(i, a, b)   for (auto i = a; i >= b; i--)
 #define all(a)          (a).begin(), (a).end()
 #define testcase        int tt;cin >> tt;while (tt--)
 #define inf             1e18
 #define _               << ' ' <<
+#define debugarray(a)   for (auto &x : a) cout << x << ' '; cout << '\n';
 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 
 typedef vector<int>                         vi;
 typedef vector<pair<int,int>>               vp;
@@ -26,10 +42,15 @@ typedef vector<double>                      vd;
 typedef vector<char>                        vc;
 typedef set<int>                            si;
 typedef set<char>                           sc;
-typedef unordered_set<int>                  usi;
+typedef unordered_set<int, custom_hash>     safe_set;
+
+
+
 
 void solve() {
     
+    
+
 }
 
 signed main() {
